0c17662a330f82a79182a5ffb5b2f3e9a949b040
GianTuka <glavinia@uade.edu.ar>
2025-11-24 14:59:31 -0300

    Cambios en la validaciond e ID, no se pueden generar id repetidos y se cambio el rango de 1000 y 5000 a 1000 y 9999

--- utilidades/utils.py
+++ utilidades/utils.py
@@ -1,14 +1,25 @@
 import random
 import os
+from data.JSONs    import leer_archivo
+from config.config import PATH_USUARIOS
  
 def generate_ID():
     """
     generate_ID genera un ID random a cada usuario registrado
  
     Returns:
-        int: un numero random [1000-5000]
+        int: un numero random [1000-9999]
     """
-    return random.randint(1000,5001)
+    usuarios = leer_archivo(PATH_USUARIOS)
+    id = random.randint(1000,10000)
+    idUsuarios = [user["ID"] for user in usuarios]
+    while True:
+        if id in idUsuarios:
+            id = random.randint(1000,10000)
+        else:
+            break
+
+    return id
 
 def pedir_entero(mensaje, minimo=None, maximo=None):
     """
